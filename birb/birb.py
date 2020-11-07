7891d6c633a2844880a1ae6d768c475246ea123f
Hycyber <cfalcon94@gmail.com>
2020-11-06 22:37:22 -0500

    Update birb.py
    Removed duplicate method

--- birb/birb.py
+++ birb/birb.py
@@ -24,21 +24,6 @@ class Birb(BaseCog):
 			else:
 				screm_list.append('a')
 		await ctx.send(''.join(screm_list1))
-        
-	@commands.command()
-	async def screm(self, ctx):
-		"""screm but more typing"""
-		screm_list = []
-		num = 0
-		range = randrange(1,50)
-		screm = random.randint(0,1)
-
-		for num in xrange(range):
-			if screm > 0:
-				screm_list.append('A')
-			else:
-				screm_list.append('a')
-		await ctx.send(''.join(screm_list))
 
 	def cog_unload(self):
 		self.bot.loop.create_task(self.session.close())
