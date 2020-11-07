e79d74b35a8d954a84d38b49e5d791b7beee08d7
Hycyber <cfalcon94@gmail.com>
2020-11-06 21:29:46 -0500

    Add files via upload
    Initial commit

---
+++ birb/birb.py
@@ -0,0 +1,54 @@
+# activate Birb
+
+import discord
+
+from redbot.core import commands
+from random import randrange
+
+BaseCog = getattr(commands, "Cog", object)
+
+class Birb(BaseCog):
+    """Birb commands."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.cooldown(1, 60, commands.BucketType.guild)
+    async def aaa(self, ctx):
+        """screm"""
+        screm_list = []
+        num = 0
+        range = randrange(1,50)
+        screm = random.randint(0,1)
+
+        for num in xrange(range):
+            if screm > 0:
+                screm_list.append('A')
+            else:
+                screm_list.append('a')
+
+        await ctx.send(''.join(screm_list))
+    @commands.command()
+    @commands.cooldown(1, 60, commands.BucketType.guild)
+    async def screm(self, ctx):
+        """screm but more typing"""
+        screm_list = []
+        num = 0
+        range = randrange(1,50)
+        screm = random.randint(0,1)
+
+        for num in xrange(range):
+            if screm > 0:
+                screm_list.append('A')
+            else:
+                screm_list.append('a')
+
+        ctx.send(''.join(screm_list))
+
+    def cog_unload(self):
+        self.bot.loop.create_task(self.session.close())
+
+    __del__ = cog_unload
+        
+        
---
+++ birb/info.jason.py
@@ -0,0 +1,10 @@
+{
+    "author" : ["Hycyber (Hycyber#7945)"],
+    "install_msg" : "Birb active.",
+    "name" : "Birb",
+    "short" : "aaaa",
+    "description" : "AAAAAAAAAAAAAAAAAA",
+    "disabled" : false,
+    "tags" : ["birb", "a", "A"],
+    "min_python_version": [3, 9, 0]
+}
