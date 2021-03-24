584d709496d59c1ae7d2ea9c92850ebe11f387e2
Hycyber <cfalcon94@gmail.com>
2021-03-23 20:44:38 -0400

    Update birb.py

--- birb/birb.py
+++ birb/birb.py
@@ -62,78 +62,77 @@ class Birb(BaseCog):
 		await ctx.send(''.join(boomer))
 		
 		
-	@commands.cooldown(1, 60, commands.BucketType.guild)
+	@commands.command()
    	async def becky(self, ctx):
-        """becky quotes"""
-        becky_list =[]
-        becky = ["This is a nice stick",
-                 "I like sticks",
-                 "Peck",
-                 "Peck",
-                 "No Ron, go find Becky",
-                 "No Ron, I don't want sum fuk",
-                 "Ugh",
-                 "Ron, your tail is small",
-                 "Ron, I'm leaving"]
+        	"""becky quotes"""
+       		becky_list =[]
+        	becky = ["This is a nice stick",
+                	"I like sticks",
+                	"Peck",
+                 	"Peck",
+                 	"No Ron, go find Becky",
+                 	"No Ron, I don't want sum fuk",
+                 	"Ugh",
+                 	"Ron, your tail is small",
+                 	"Ron, I'm leaving"]
         
-        num = 0
-        range = randrange(1,15)
+        	num = 0
+        	range = randrange(1,5)
 
-        for num in xrange(range):
-            bird = random.randint(0,1)
-            if bird > 0:
-                becky_list.append(random.choice(becky))
-                becky_list.append('\n')
-            else:
-                becky_list.append('stik')
-                becky_list.append('\n')
+        	for num in xrange(range):
+            		bird = random.randint(0,1)
+            		if bird > 0:
+                		becky_list.append(random.choice(becky))
+                		becky_list.append('\n')
+            		else:
+                		becky_list.append('stik')
+                		becky_list.append('\n')
 
-        await ctx.send(''.join(becky_list))
-    @commands.command()
-    
-    @commands.cooldown(1, 60, commands.BucketType.guild)
-    async def ron(self, ctx):
-        """ron quotes"""
-        ron_list =[]
-        ron = ["Lemme smash",
-               "Please",
-               "You want, sum fuk?",
-               "I got you blue",
-               "Hey gurl, you want some tail?",
-               "Wot?",
-               "Swiggity, swooty",
-               "You want yellow?",
-               "She doesn't want yellow",
-               "Blue and yellow?",
-               "Wut?",
-               "No wait lemme smash",
-               "What has my life come to?",
-               "Becky thought my tail was big",
-               "Becky used to let me smash. But Becky is smashing Ben",
-               "Ben is a hoe",
-               "FUCK THIS NEST, FUCK BEN",
-               "I NEED YOU BECKY",
-               "BECKY LEMME SMASH",
-               "Imma get that bitch a stick. Bitches love sticks",
-               "Wrong stick",
-               "Stick",
-               "Need stick",
-               "Got stick",
-               "Becky, I got stick",
-               "LEMME SMASH"]
-        num = 0
-        range = randrange(1,15)
+        	await ctx.send(''.join(becky_list))
 
-        for num in xrange(range):
-            bird = random.randint(0,1)
-            if bird > 0:
-                ron_list.append(random.choice(ron))
-                ron_list.append('\n')
-            else:
-                ron_list.append('stik')
-                ron_list.append('\n')
+    	@commands.command()
+   	async def ron(self, ctx):
+        	"""ron quotes"""
+       		ron_list =[]
+        	ron =  ["Lemme smash",
+               		"Please",
+               		"You want, sum fuk?",
+               		"I got you blue",
+               		"Hey gurl, you want some tail?",
+               		"Wot?",
+               		"Swiggity, swooty",
+               		"You want yellow?",
+               		"She doesn't want yellow",
+               		"Blue and yellow?",
+               		"Wut?",
+               		"No wait lemme smash",
+               		"What has my life come to?",
+               		"Becky thought my tail was big",
+               		"Becky used to let me smash. But Becky is smashing Ben",
+               		"Ben is a hoe",
+               		"FUCK THIS NEST, FUCK BEN",
+               		"I NEED YOU BECKY",
+               		"BECKY LEMME SMASH",
+               		"Imma get that bitch a stick. Bitches love sticks",
+               		"Wrong stick",
+               		"Stick",
+               		"Need stick",
+               		"Got stick",
+               		"Becky, I got stick",
+               		"LEMME SMASH"]
+       		num = 0
+        	range = randrange(1,15)
 
-        await ctx.send(''.join(ron_list))
+        	for num in xrange(range):
+            		bird = random.randint(0,1)
+            		if bird > 0:
+                		ron_list.append(random.choice(ron))
+                		ron_list.append('\n')
+            		else:
+                		ron_list.append('stik')
+                		ron_list.append('\n')
+
+        	await ctx.send(''.join(ron_list))
 
 	def cog_unload(self):
 		self.bot.loop.create_task(self.session.close())
