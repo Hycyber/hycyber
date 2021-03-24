b0ed3cf86ed2c03649c45f03ec66d77b5a7597da
Hycyber <cfalcon94@gmail.com>
2021-03-23 20:16:54 -0400

    Initial commit

---
+++ becky/_init_.py
@@ -0,0 +1,4 @@
+from .becky import Becky
+
+def setup(bot):
+    bot.add_cog(Becky(bot)
---
+++ becky/info.jason.py
@@ -0,0 +1,10 @@
+{
+    "author" : ["Hycyber (Hycyber#7945)"],
+    "install_msg" : "Becky, ready to smash.",
+    "name" : "Becky",
+    "short" : "Add some Becky to your life",
+    "description" : "You wan sum stik",
+    "disabled" : false,
+    "tags" : ["Becky", "lemme", "smash"],
+    "min_python_version": [3, 9, 0]
+}
