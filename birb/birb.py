3a0aec78149eacb76f87935f544858def2841a6b
Hycyber <cfalcon94@gmail.com>
2021-06-05 21:42:10 -0400

    Update birb.py
    added !drink and !liqa commands. liqa is in beta, ideally would like an api in the future

--- birb/birb.py
+++ birb/birb.py
@@ -214,6 +214,54 @@ class Birb(BaseCog):
 	async def fish(self, ctx):
 		await ctx.send("https://tenor.com/view/pog-fish-fish-mouth-open-gif-17487624")
 		
+ #!drink
+	@commands.command()
+	async def drink(self, ctx):
+		await ctx.send("!play https://www.youtube.com/watch?v=f55CqLc6IR0")
+		
+     #!liqa
+	@commands.command()
+	async def liqa(self, ctx):
+		"""tells you what to get from the store"""
+		drink_list =[]
+		drink_list.append("Currently in beta, more choices to come later")
+		drink_list.append('\n')
+		bourbon =  ["RUSSELL'S RESERVE 6YR RYE: https://www.totalwine.com/spirits/bourbon/small-batch-bourbon/russells-reserve-6yr-rye/p/46841750?s=801&igrules=true",
+			"JIM BEAM SINGLE BARREL 108 PROOF: https://www.totalwine.com/spirits/bourbon/small-batch-bourbon/jim-beam-single-barrel-108-proof/p/224249750?s=801&igrules=true",
+			"VIRGIL KAINE GINGER INFUSED BOURBON: https://www.totalwine.com/spirits/bourbon/virgil-kaine-ginger-infused-bourbon/p/121349750?s=801&igrules=true",
+			"BIRD DOG KENTUCKY STRAIGHT BOURBON 84 PROOF: https://www.totalwine.com/spirits/bourbon/small-batch-bourbon/bird-dog-kentucky-straight-bourbon-84-proof/p/231881750?s=801&igrules=true",
+			"ASW FIDDLER UNISON BOURBON: https://www.totalwine.com/spirits/bourbon/asw-fiddler-unison-bourbon/p/190755750?s=801&igrules=true",
+			"HIRSCH HORIZON BOURBON: https://www.totalwine.com/spirits/bourbon/small-batch-bourbon/hirsch-horizon-bourbon/p/230979750?s=801&igrules=true",
+			"VERY OLD BARTON 90 PF: https://www.totalwine.com/spirits/bourbon/very-old-barton-90-pf/p/169772175?s=801&igrules=true",
+			"REDWOOD EMPIRE PIPE DREAM WHISKEY: https://www.totalwine.com/spirits/bourbon/redwood-empire-pipe-dream-whiskey/p/184407750?s=801&igrules=true",
+			"PINHOOK FLAGSHIP BOURBON WHISKEY: https://www.totalwine.com/spirits/bourbon/small-batch-bourbon/pinhook-flagship-bourbon-whiskey/p/232301750?s=801&igrules=true",
+			"JACK DANIELS LEGACY EDITION 2: https://www.totalwine.com/spirits/bourbon/jack-daniels-legacy-edition-2/p/191358750?s=801&igrules=true"]
+		brandy = ["ABK6 VSOP COGNAC: https://www.totalwine.com/spirits/brandy-cognac/cognac/abk6-vsop-cognac/p/135127750?s=801&igrules=true",
+			"1889 ROYAL BRANDY: https://www.totalwine.com/spirits/brandy-cognac/brandy/1889-royal-brandy/p/171047175?s=801&igrules=true,
+			"E & J BRANDY: https://www.totalwine.com/spirits/brandy-cognac/brandy/e-j-brandy/p/1007175?s=801&igrules=true",
+			"KORBEL BRANDY: https://www.totalwine.com/spirits/brandy-cognac/brandy/korbel-brandy/p/5736175?s=801&igrules=true",
+			"FIDELITAS KIRSCHWASSER: https://www.totalwine.com/spirits/brandy-cognac/eau-de-vie/fidelitas-kirschwasser/p/115921750?s=801&igrules=true",
+			"CHATEAU DE LAUBADE SIGNATURE ARMAGNAC: https://www.totalwine.com/spirits/brandy-cognac/armagnac/chateau-de-laubade-signature-armagnac/p/117663750?s=801&igrules=true",
+			"MAURO SEBASTE MOSCATO GRAPPA: https://github.com/Hycyber/hycyber/edit/main/birb/birb.py",
+			"BANKERS CLUB BRANDY: https://www.totalwine.com/spirits/brandy-cognac/brandy/bankers-club-brandy/p/7587175?s=801&igrules=true",
+			"CHATEAU DE LACQUY REFERENCE: https://www.totalwine.com/spirits/brandy-cognac/armagnac/chateau-de-lacquy-reference/p/226866750?s=801&igrules=true",
+			"D'USSE COGNAC VSOP: https://www.totalwine.com/spirits/brandy-cognac/cognac/dusse-cognac-vsop/p/130477375?s=801&igrules=true"]
+		
+		x = randrange(1,2)
+		
+		if(x == 1):
+			drink_list.append("Tonight's choice is burbon: ")
+			drink_list.append('\n')
+			drink_list.append(random.choice(burbon))
+			
+		if(x == 2):
+			drink_list.append("Tonight's choice is brandy/cognac: ")
+			drink_list.append('\n')
+			drink_list.append(random.choice(brandy))
+		
+	
+		await ctx.send(''.join(drink_list))
+		
 #End Hycyber's designed commands
 	def cog_unload(self):
 		self.bot.loop.create_task(self.session.close())
